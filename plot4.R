f2c4a5c00ffce6112c97094574760815026373ca
NikolasEnt <Nikolasent@gmail.com>
2016-01-24 17:39:47 +0300

    Plot 4 corrected

--- plot4.R
+++ plot4.R
@@ -9,6 +9,7 @@ plot(sub_inf$timestamp, sub_inf$Sub_metering_1, type="l", xlab="", ylab="Energy 
 lines(sub_inf$timestamp, sub_inf$Sub_metering_2,col="red")
 lines(sub_inf$timestamp, sub_inf$Sub_metering_3,col="blue")
 legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5)
-plot(sub_inf$timestamp,sub_inf$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(sub_inf$timestamp,sub_inf$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power", yaxt='n')
+axis(2, at = seq(0.0, 0.5, by = 0.1), las=2)
 dev.copy(png, file="plot4.png", width=480, height=480)
 dev.off()
